436751ad4c844c2b1659b509caeb00be5cc004ee
Robertimor <dybina0012@mail.ru>
2025-04-01 15:01:30 +0300

    Fix server.js

--- server.js
+++ server.js
@@ -19,9 +19,31 @@ app.get("/getApiKey", (req, res) => {
   res.json({ apiKey: process.env.VITE_API_KEY });
 });
 
+// app.get("/geonames", async (req, res) => {
+//   res.json({ apiKey: process.env.VITE_UNSPLASH_KEY })
+// })
+
 app.get("/geonames", async (req, res) => {
-  res.json({ apiKey: process.env.VITE_UNSPLASH_KEY })
-})
+  const { city, lang } = req.query;
+  const username = "robertimor"; // Используется как API-ключ в Geonames
+
+  if (!city || !lang) {
+    return res.status(400).json({ error: "Missing required parameters: city or lang" });
+  }
+
+  const apiUrl = `http://api.geonames.org/searchJSON?q=${city}&lang=${lang}&maxRows=1&username=${username}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) throw new Error("Ошибка запроса к Geonames");
+
+    const data = await response.json();
+    res.json(data); // Отправляем ответ с Geonames
+  } catch (error) {
+    console.error("Ошибка при запросе к Geonames API:", error);
+    res.status(500).json({ error: "Ошибка при запросе к Geonames API" });
+  }
+});
 
 // app.get("/geonames", async (req, res) => {
 //   // const { city, lang } = req.query;
